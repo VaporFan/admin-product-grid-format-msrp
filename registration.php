e4543631e2fbea220f90b18d4629e0deb7070c02
root <root@flamevn-s01.asia-southeast1-b.c.flame-223302.internal>
2019-01-30 03:11:11 +0000

    first commit

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Gft_AdminProductGridFormatMsrp',
+        __DIR__
+        );
